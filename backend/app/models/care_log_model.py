3f0fc542493d8ba41be0f6504dbeadf11e32611c
Phu1401 <2251162110@e.tlu.edu.vn>
2026-01-16 14:56:59 +0700

    Fix import Date trong care_log_model

--- backend/app/models/care_log_model.py
+++ backend/app/models/care_log_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, Float, Date, DateTime, ForeignKey, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database.database import Base
